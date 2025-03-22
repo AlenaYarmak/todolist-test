b8cfe81bc329d90f4acc9b7f039cbcd84236bab3
Alena <olena.yarmak.mail@gmail.com>
2025-03-22 16:02:04 +0100

    add mocked data / add TaskList

---
+++ src/components/TaskList.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import TaskItem from './TaskItem';
+import tasksData from './../data/tasksMockData.json';
+import { Task } from '../types/types';
+
+interface TaskListProps {
+    tasks: Task[];
+    setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+    clearCompleted: () => void;
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, setTasks, clearCompleted }) => {
+    const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+    const activeTaskCounter = tasksData.tasks.filter(task => task.status === 'active').length;
+    
+    const filteredTasks = tasks.filter(task => {
+        if (filter === 'all') return true;
+        return task.status === filter;
+    });
+
+    const toggleTaskStatus = (id: string | number) => {
+        setTasks(tasks.map(task => 
+          task.id === id 
+            ? {...task, status: task.status === 'active' ? 'completed' : 'active'} 
+            : task
+        ));
+      };
+
+    return (
+        <div className='task-list'>
+            {filteredTasks.map((task) => (
+                <TaskItem 
+                    task={task}
+                    key={task.id}
+                    onToggle={toggleTaskStatus}
+                />
+            ))}
+            <div className='task-container'>
+                <div className='task-counter'>
+                    {activeTaskCounter} items left
+                </div>
+                <div 
+                    className='task-clear'
+                    onClick={clearCompleted}>
+                        Clear Completed
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default TaskList;
